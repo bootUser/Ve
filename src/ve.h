0f73af37cf309809592d8dca945f745a06309716
bootUser <slip2015syper@gmail.com>
2023-11-09 19:27:01 +0300

    Найдены первые крупицы языка В

---
+++ src/ve.h
@@ -0,0 +1,55 @@
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define вперёд_славяне setlocale(LC_ALL, "")
+
+#define царь_батюшка_главный int main
+#define молвить printf
+#define внемлить scanf
+#define воздать return
+
+#define пахать do
+#define покуда while
+#define для for
+
+#define коли if
+#define отнюдь else
+
+#define семейство struct
+#define перепись enum
+
+#define бить_ящеров break
+#define добить_ящеров continue
+#define путевой_камень switch
+#define путь case
+#define базированно default
+
+#define правда 1
+#define кривда 0
+
+#define туда_не_знаю_куда NULL
+#define приказ_княжий const
+#define мощь pow
+#define окстись goto
+#define встрой inline
+#define кощей static
+#define однобокий unsigned
+#define суд_Перуна auto
+#define мечущийся volatile
+
+typedef char многа_букав[];
+typedef int целина;
+typedef double вель_дробь;
+typedef float малый_дробь;
+typedef char буква;
+typedef void бестолочь;
+typedef short карлик;
+typedef long долговязый;
+typedef long long Петр_Первый;
+typedef size_t мерило;
+
+
+перепись счёт_древних_русов{ ноль = 0, целковый = 1, полушка = 2, четвертушка = 3, осьмушка = 4,
+пудовичок = 5, медячок = 6, серебрячок = 7, золотничок = 8, девятичок = 9, десятичок = 10 };
